2eadcda8ababf8c020cc9b2269b4ad404084b490
eganpg <eganpg@gmail.com>
2014-07-28 14:12:34 -0700

    first commit of new company project

---
+++ Company_Goals.md
@@ -0,0 +1,45 @@
+What are the goals:
+
+Create an online ordering platform which purveyors can provide to restaurants to increase sales and marketshare for specific company facilitate the ordering process
+
+
+
+What does this need to do:
+
+	Customer(Chef/Restaurant Manager):
+		** Mimic an ordering clip board that generates an order ** 
+		** Provide easily searchable product database **
+
+		Provide easy way to create order guide
+
+		Easy way to update order
+		
+		In one click send & process an order
+		Allow restaurant to adequately check in order
+		Keep order history
+
+	Purveyor:	
+		Provide business analytics tools to give purveyor insightful tools to grow business client specifically
+
+		Way to suggest purchase add on's/limited time items/specials
+
+		Way to capture business from other purveryors, consolidating orders
+
+		Provide more direct communication with clients
+
+		Position purveyors in a virtual marketplace where produce purchase can be seamlessly alongside meat/fish ect..
+
+
+	Major Competition:
+		Advantages:
+			SyscoCounts App: a proprietary ordering app that allows sysco clients to order online from sysco.
+		Disadvantages: 
+			App makes it very difficult to order from anyone but Sysco
+
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,54 @@
+var newCompany = angular.module('newCompany', [])
+	newCompany.controller('searchContainer', function($scope){
+		$scope.productList = [
+			{	name: 'Beans',
+				descript: 'The Greatest Beans on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{
+				name: 'Eggs',
+				descript: 'The Greatest Eggs on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			},
+			{	name: 'Soup',
+				descript: 'The Greatest Soup on Earth',
+				price: '5.00'
+			}
+		]
+	});
