5881ae15a5b4ef3910bd253099c609b08bfe711d
eganpg <eganpg@gmail.com>
2014-08-02 15:23:24 -0700

    Commiting with angular update nothing really new

---
+++ orderboard.js
@@ -0,0 +1,16 @@
+var orderboard = angular.module('orderboard', []);
+orderboard.controller('orderguide', function($scope) {
+$scope.product = [
+						{name: 'Fennel'},
+						{name: 'Lettuce'},
+						{name: 'Celery'},
+						{name: 'Carrots'},
+						{name: 'Beets'},
+						{name: 'Tarragon'},
+						{name: 'Parsley'},
+						{name: 'Beans'},
+						{name: 'Beans'},
+						{name: 'Beans'}
+						
+]
+});
